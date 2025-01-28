bba5eba03739c35e1fe58ec02daf56fae8b38769
Ahmad Hafe <ahafe@redhat.com>
2025-01-28 23:03:57 +0200

    whitelist alert SSPCommonTemplatesModificationReverted on healthy cluster (#195)

--- tests/observability/alerts/utils.py
+++ tests/observability/alerts/utils.py
@@ -3,8 +3,10 @@ import logging
 from kubernetes.dynamic.exceptions import ResourceNotFoundError
 from ocp_resources.namespace import Namespace
 
-KUBEVIRT_HYPERCONVERGED_OPERATOR_HEALTH_STATUS = "kubevirt_hyperconverged_operator_health_status"
+from tests.observability.alerts.constants import SSP_COMMON_TEMPLATES_MODIFICATION_REVERTED
 
+KUBEVIRT_HYPERCONVERGED_OPERATOR_HEALTH_STATUS = "kubevirt_hyperconverged_operator_health_status"
+ALLOW_ALERTS_ON_HEALTHY_CLUSTER_LIST = [SSP_COMMON_TEMPLATES_MODIFICATION_REVERTED]
 CONTAINERIZED_DATA_IMPORTER = "containerized-data-importer"
 LOGGER = logging.getLogger(__name__)
 
@@ -17,8 +19,9 @@ def verify_no_listed_alerts_on_cluster(prometheus, alerts_list):
     for alert in alerts_list:
         alerts_by_name = prometheus.get_all_alerts_by_alert_name(alert_name=alert)
         if alerts_by_name and alerts_by_name[0]["state"] == "firing":
+            if alert in ALLOW_ALERTS_ON_HEALTHY_CLUSTER_LIST:
+                continue
             fired_alerts[alert] = alerts_by_name
-
     assert not fired_alerts, f"Alerts should not be fired on healthy cluster.\n {fired_alerts}"
 
 
