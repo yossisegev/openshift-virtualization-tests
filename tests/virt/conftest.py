9c1af3f4d2216a3786d16c54033b17e0b76a3fd1
vsibirsk <57763370+vsibirsk@users.noreply.github.com>
2025-03-17 16:44:09 +0200

    [virt] update virt-sanity checks (#489) (#506)
    * [virt] update virt-sanity checks
    
        - add pytest skip parameter (--skip-virt-sanity-check)
        - split validations per collected marker
        - replace the validation trigger from pytest -m option to based on collection
    
    * remove numa from special_infra
    
        - removed special_infra marker from numa
        - removed numa from virt-sanity validation
        - removed numa from tier2 collection
    
    * moved conditions (reduce conditions calls count)

--- tests/virt/conftest.py
+++ tests/virt/conftest.py
@@ -5,7 +5,7 @@ from bitmath import parse_string_unsafe
 from ocp_resources.performance_profile import PerformanceProfile
 
 from utilities.constants import AMD, INTEL
-from utilities.infra import ExecCommandOnPod, exit_pytest_execution
+from utilities.infra import exit_pytest_execution
 from utilities.virt import get_nodes_gpu_info
 
 LOGGER = logging.getLogger(__name__)
@@ -13,13 +13,12 @@ LOGGER = logging.getLogger(__name__)
 
 @pytest.fixture(scope="session", autouse=True)
 def virt_special_infra_sanity(
-    pytestconfig,
+    request,
     junitxml_plugin,
     is_psi_cluster,
     schedulable_nodes,
     gpu_nodes,
     nodes_with_supported_gpus,
-    workers_utility_pods,
     sriov_workers,
     workers,
 ):
@@ -42,29 +41,17 @@ def virt_special_infra_sanity(
         if not _nodes_with_supported_gpus:
             failed_verifications_list.append("Cluster doesn't have any nodes with supported GPUs")
         if len(_nodes_with_supported_gpus) < 2:
-            failed_verifications_list.append("Cluster has only 1 node with GPU")
+            failed_verifications_list.append(f"Cluster has only {len(_nodes_with_supported_gpus)} node with GPU")
 
     def _verfify_no_dpdk():
         LOGGER.info("Verify cluster doesn't have DPDK enabled")
         if PerformanceProfile(name="dpdk").exists:
             failed_verifications_list.append("Cluster has DPDK enabled (DPDK is incomatible with NVIDIA GPU)")
 
-    def _verify_numa(_schedulable_nodes, _workers_utility_pods):
-        LOGGER.info("Verify cluster has NUMA")
-        cat_cmd = "cat /etc/kubernetes/kubelet.conf"
-        single_numa_node_cmd = f"{cat_cmd} | grep -i single-numa-node"
-        topology_manager_cmd = f"{cat_cmd} | grep -w TopologyManager"
-        for cmd in (single_numa_node_cmd, topology_manager_cmd):
-            for node in _schedulable_nodes:
-                pod_exec = ExecCommandOnPod(utility_pods=_workers_utility_pods, node=node)
-                out = pod_exec.exec(command=cmd, ignore_rc=True)
-                if not out:
-                    failed_verifications_list.append(f"Cluster does not have {cmd.split()[-1]} enabled")
-
     def _verify_sriov(_sriov_workers):
         LOGGER.info("Verify cluster has worker node with SR-IOV card")
         if not _sriov_workers:
-            failed_verifications_list.append("Cluster doesn't have any SR-IOV workers")
+            failed_verifications_list.append("Cluster does not have any SR-IOV workers")
 
     def _verify_evmcs_support(_schedulable_nodes):
         LOGGER.info("Verify cluster nodes support VMX cpu fixture")
@@ -72,7 +59,7 @@ def virt_special_infra_sanity(
             if not any([
                 label == "cpu-feature.node.kubevirt.io/vmx" and value == "true" for label, value in node.labels.items()
             ]):
-                failed_verifications_list.append("Cluster doens't have any node that supports VMX cpu feature")
+                failed_verifications_list.append("Cluster does not have any node that supports VMX cpu feature")
 
     def _verify_hugepages_1gi(_workers):
         LOGGER.info("Verify cluster has 1Gi hugepages enabled")
@@ -80,20 +67,27 @@ def virt_special_infra_sanity(
             parse_string_unsafe(worker.instance.status.allocatable["hugepages-1Gi"]) >= parse_string_unsafe("1Gi")
             for worker in _workers
         ]):
-            failed_verifications_list.append("Cluster doesn't have hugepages-1Gi")
+            failed_verifications_list.append("Cluster does not have hugepages-1Gi")
 
+    skip_virt_sanity_check = "--skip-virt-sanity-check"
     failed_verifications_list = []
 
-    if "special_infra" in pytestconfig.getoption("-m"):
+    if not request.session.config.getoption(skip_virt_sanity_check):
         LOGGER.info("Verifying that cluster has all required capabilities for special_infra marked tests")
-        _verify_not_psi_cluster(_is_psi_cluster=is_psi_cluster)
-        _verify_cpumanager_workers(_schedulable_nodes=schedulable_nodes)
-        _verify_gpu(_gpu_nodes=gpu_nodes, _nodes_with_supported_gpus=nodes_with_supported_gpus)
-        _verfify_no_dpdk()
-        _verify_numa(_schedulable_nodes=schedulable_nodes, _workers_utility_pods=workers_utility_pods)
-        _verify_sriov(_sriov_workers=sriov_workers)
-        _verify_evmcs_support(_schedulable_nodes=schedulable_nodes)
-        _verify_hugepages_1gi(_workers=workers)
+        if any(item.get_closest_marker("high_resource_vm") for item in request.session.items):
+            _verify_not_psi_cluster(_is_psi_cluster=is_psi_cluster)
+            _verify_evmcs_support(_schedulable_nodes=schedulable_nodes)
+        if any(item.get_closest_marker("cpu_manager") for item in request.session.items):
+            _verify_cpumanager_workers(_schedulable_nodes=schedulable_nodes)
+        if any(item.get_closest_marker("gpu") for item in request.session.items):
+            _verify_gpu(_gpu_nodes=gpu_nodes, _nodes_with_supported_gpus=nodes_with_supported_gpus)
+            _verfify_no_dpdk()
+        if any(item.get_closest_marker("sriov") for item in request.session.items):
+            _verify_sriov(_sriov_workers=sriov_workers)
+        if any(item.get_closest_marker("hugepages") for item in request.session.items):
+            _verify_hugepages_1gi(_workers=workers)
+    else:
+        LOGGER.warning(f"Skipping virt special infra sanity because {skip_virt_sanity_check} was passed")
 
     if failed_verifications_list:
         err_msg = "\n".join(failed_verifications_list)
