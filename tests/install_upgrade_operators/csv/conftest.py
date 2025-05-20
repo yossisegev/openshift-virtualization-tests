be461ca945af0b3062d2cf657335832b1259a972
cnv-qe-bot1 <168687541+cnv-qe-bot1@users.noreply.github.com>
2025-05-20 12:34:51 -0400

    [IUO] Align packagemanifest tests with channels (#952) (#992)
    Signed-off-by: Harel Meir <hmeir@redhat.com>
    Co-authored-by: Harel Meir <hmeir@redhat.com>
    Co-authored-by: vsibirsk <57763370+vsibirsk@users.noreply.github.com>

--- tests/install_upgrade_operators/csv/conftest.py
+++ tests/install_upgrade_operators/csv/conftest.py
@@ -3,37 +3,31 @@ import logging
 import pytest
 from pytest_testconfig import py_config
 
-from tests.install_upgrade_operators.csv.utils import (
-    KubevirtManifestChannelNotFoundError,
-)
 from utilities.infra import get_kubevirt_package_manifest
 
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture()
-def kubevirt_package_manifest(admin_client):
-    """
-    Find kubevirt raw package manifest associated with hco-catalogsource.
-    """
-    package_manifest = get_kubevirt_package_manifest(admin_client=admin_client)
-    assert package_manifest, f"Package manifest{py_config['hco_cr_name']} not found"
-    return package_manifest
+class KubevirtManifestChannelNotFoundError(Exception):
+    pass
 
 
-@pytest.fixture()
-def kubevirt_package_manifest_channel(kubevirt_package_manifest, cnv_current_version):
-    """
-    Return channel name from Kubevirt Package Manifest.
-    """
-    channels = kubevirt_package_manifest.status.channels
-    for channel in channels:
+@pytest.fixture(scope="package")
+def kubevirt_package_manifest_channels(admin_client):
+    kubevirt_package_manifest = get_kubevirt_package_manifest(admin_client=admin_client)
+    assert kubevirt_package_manifest, f"Package manifest {py_config['hco_cr_name']} not found"
+    return kubevirt_package_manifest.status.channels
+
+
+@pytest.fixture(scope="package")
+def kubevirt_package_manifest_current_channel(kubevirt_package_manifest_channels, cnv_current_version):
+    for channel in kubevirt_package_manifest_channels:
         if channel.currentCSVDesc.version == cnv_current_version:
-            LOGGER.info(f"Getting channel associated with cnv version: {cnv_current_version}")
-            return channel.name
+            LOGGER.info(f"Channel {channel.name} is associated with cnv version: {cnv_current_version}")
+            return channel
     raise KubevirtManifestChannelNotFoundError(
         f"Not able to find channel matching {cnv_current_version} in the package manifest."
-        f" Avaliable channels: {channels}"
+        f"Available channels: {[channel.name for channel in kubevirt_package_manifest_channels]}"
     )
 
 
