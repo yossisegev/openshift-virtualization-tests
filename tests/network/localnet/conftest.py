0bf89885bb7990dcb19734e6bb64ef274e235c50
‪Anat Wax <46938838+Anatw@users.noreply.github.com>
2025-05-20 17:32:39 +0300

    Localnet scenario - connectivity over migration on additional bridg (#994)
    This commit updates the localnet tests to use the new
    'create_traffic_server' and 'create_traffic_client' functions from the
    'liblocalnet' module. The existing Server and Client fixtures have been
    replaced with these new functions to improve test readability and
    maintainability.
    
    Additionally, the directory now contain 2 modules: one for testing the
    default (br-ex) bridge and another to test the additional ovs bridge
    scenarios. Each module contain a test for verifying connectivity between
    localnet VMs before and after migration.
    
    The existing localnet scenario
    (test_connectivity_over_migration_between_localnet_vms - CNV-11775) was
    updated to use the new functions, while the new
    test_connectivity_over_migration_between_ovs_bridge_localnet_vms
    scenario was added to demonstrate connectivity between localnet VMs
    using an additional bridge.
    
    Signed-off-by: Anat Wax <awax@redhat.com>

--- tests/network/localnet/conftest.py
+++ tests/network/localnet/conftest.py
@@ -2,19 +2,27 @@ from collections.abc import Generator
 
 import pytest
 from ocp_resources.namespace import Namespace
+from ocp_resources.node import Node
 
 import tests.network.libs.nodenetworkconfigurationpolicy as libnncp
 from libs.net import netattachdef
 from libs.net.traffic_generator import Client, Server
-from libs.net.vmspec import IP_ADDRESS, lookup_iface_status
 from libs.vm.vm import BaseVirtualMachine
-from tests.network.localnet.liblocalnet import NETWORK_NAME, localnet_nad, localnet_vm
+from tests.network.localnet.liblocalnet import (
+    create_traffic_client,
+    create_traffic_server,
+    localnet_nad,
+    localnet_vm,
+    run_vms,
+)
 from utilities.constants import (
     WORKER_NODE_LABEL_KEY,
 )
 from utilities.infra import create_ns
 
-_IPERF_SERVER_PORT = 5201
+LOCALNET_BR_EX_NETWORK = "localnet-br-ex-network"
+LOCALNET_OVS_BRIDGE_NETWORK = "localnet-ovs-network"
+NNCP_INTERFACE_TYPE_OVS_BRIDGE = "ovs-bridge"
 
 
 @pytest.fixture(scope="module")
@@ -22,7 +30,7 @@ def nncp_localnet() -> Generator[libnncp.NodeNetworkConfigurationPolicy]:
     desired_state = libnncp.DesiredState(
         ovn=libnncp.OVN([
             libnncp.BridgeMappings(
-                localnet=NETWORK_NAME,
+                localnet=LOCALNET_BR_EX_NETWORK,
                 bridge=libnncp.DEFAULT_OVN_EXTERNAL_BRIDGE,
                 state=libnncp.BridgeMappings.State.PRESENT.value,
             )
@@ -57,7 +65,12 @@ def vlan_id(vlan_index_number: Generator[int]) -> int:
 def nad_localnet_1(
     namespace_localnet_1: Namespace, vlan_id: int
 ) -> Generator[netattachdef.NetworkAttachmentDefinition]:
-    with localnet_nad(namespace=namespace_localnet_1.name, name="test-localnet-nad1", vlan_id=vlan_id) as nad:
+    with localnet_nad(
+        namespace=namespace_localnet_1.name,
+        name="test-localnet-nad1",
+        vlan_id=vlan_id,
+        network_name=LOCALNET_BR_EX_NETWORK,
+    ) as nad:
         yield nad
 
 
@@ -65,54 +78,183 @@ def nad_localnet_1(
 def nad_localnet_2(
     namespace_localnet_2: Namespace, vlan_id: int
 ) -> Generator[netattachdef.NetworkAttachmentDefinition]:
-    with localnet_nad(namespace=namespace_localnet_2.name, name="test-localnet-nad2", vlan_id=vlan_id) as nad:
+    with localnet_nad(
+        namespace=namespace_localnet_2.name,
+        name="test-localnet-nad2",
+        vlan_id=vlan_id,
+        network_name=LOCALNET_BR_EX_NETWORK,
+    ) as nad:
         yield nad
 
 
 @pytest.fixture(scope="module")
-def vm_localnet_1(nad_localnet_1: netattachdef.NetworkAttachmentDefinition) -> Generator[BaseVirtualMachine]:
+def ipv4_localnet_address_pool() -> Generator[str]:
+    return (f"10.0.0.{host_value}/24" for host_value in range(1, 254))
+
+
+@pytest.fixture(scope="module")
+def vm_localnet_1(
+    ipv4_localnet_address_pool: Generator[str], nad_localnet_1: netattachdef.NetworkAttachmentDefinition
+) -> Generator[BaseVirtualMachine]:
     with localnet_vm(
-        namespace=nad_localnet_1.namespace, name="test-vm1", network=nad_localnet_1.name, cidr="10.0.0.1/24"
+        namespace=nad_localnet_1.namespace,
+        name="test-vm1",
+        nad_name=nad_localnet_1.name,
+        cidr=next(ipv4_localnet_address_pool),
+        spec_logical_network=LOCALNET_BR_EX_NETWORK,
     ) as vm:
         yield vm
 
 
 @pytest.fixture(scope="module")
-def vm_localnet_2(nad_localnet_2: netattachdef.NetworkAttachmentDefinition) -> Generator[BaseVirtualMachine]:
+def vm_localnet_2(
+    ipv4_localnet_address_pool: Generator[str], nad_localnet_2: netattachdef.NetworkAttachmentDefinition
+) -> Generator[BaseVirtualMachine]:
     with localnet_vm(
-        namespace=nad_localnet_2.namespace, name="test-vm2", network=nad_localnet_2.name, cidr="10.0.0.2/24"
+        namespace=nad_localnet_2.namespace,
+        name="test-vm2",
+        nad_name=nad_localnet_2.name,
+        cidr=next(ipv4_localnet_address_pool),
+        spec_logical_network=LOCALNET_BR_EX_NETWORK,
     ) as vm:
         yield vm
 
 
 @pytest.fixture(scope="module")
-def vms_localnet(
+def server_client_localnet_vms(
     vm_localnet_1: BaseVirtualMachine, vm_localnet_2: BaseVirtualMachine
 ) -> Generator[tuple[BaseVirtualMachine, BaseVirtualMachine]]:
-    vms = (vm_localnet_1, vm_localnet_2)
-    for vm in vms:
-        vm.start()
-    for vm in vms:
-        vm.wait_for_ready_status(status=True)
-        vm.vmi.wait_for_condition(condition=vm.Condition.Type.AGENT_CONNECTED, status=vm.Condition.Status.TRUE)
-    yield vms
+    server_vm, client_vm = run_vms(vms=(vm_localnet_1, vm_localnet_2))
+    yield (server_vm, client_vm)
 
 
 @pytest.fixture()
-def localnet_server(vms_localnet: tuple[BaseVirtualMachine, BaseVirtualMachine]) -> Generator[Server]:
-    vm1, _ = vms_localnet
-    with Server(vm=vm1, port=_IPERF_SERVER_PORT) as server:
+def localnet_server(server_client_localnet_vms: tuple[BaseVirtualMachine, BaseVirtualMachine]) -> Generator[Server]:
+    with create_traffic_server(vm=server_client_localnet_vms[0]) as server:
         assert server.is_running()
         yield server
 
 
 @pytest.fixture()
-def localnet_client(vms_localnet: tuple[BaseVirtualMachine, BaseVirtualMachine]) -> Generator[Client]:
-    vm1, vm2 = vms_localnet
-    with Client(
-        vm=vm2,
-        server_ip=lookup_iface_status(vm=vm1, iface_name=NETWORK_NAME)[IP_ADDRESS],
-        server_port=_IPERF_SERVER_PORT,
+def localnet_client(server_client_localnet_vms: tuple[BaseVirtualMachine, BaseVirtualMachine]) -> Generator[Client]:
+    with create_traffic_client(
+        server_vm=server_client_localnet_vms[0],
+        client_vm=server_client_localnet_vms[1],
+        network_name=LOCALNET_BR_EX_NETWORK,
+    ) as client:
+        assert client.is_running()
+        yield client
+
+
+@pytest.fixture(scope="module")
+def nncp_localnet_on_secondary_node_nic(
+    worker_node1: Node, nodes_available_nics: dict[str, list[str]]
+) -> Generator[libnncp.NodeNetworkConfigurationPolicy]:
+    bridge_name = "localnet-ovs-br"
+    desired_state = libnncp.DesiredState(
+        interfaces=[
+            libnncp.Interface(
+                name=bridge_name,
+                type=NNCP_INTERFACE_TYPE_OVS_BRIDGE,
+                ipv4=libnncp.IPv4(enabled=False),
+                ipv6=libnncp.IPv6(enabled=False),
+                state=libnncp.Resource.Interface.State.UP,
+                bridge=libnncp.Bridge(
+                    options=libnncp.BridgeOptions(libnncp.STP(enabled=False)),
+                    port=[
+                        libnncp.Port(
+                            name=nodes_available_nics[worker_node1.name][-1],
+                        )
+                    ],
+                ),
+            )
+        ],
+        ovn=libnncp.OVN([
+            libnncp.BridgeMappings(
+                localnet=LOCALNET_OVS_BRIDGE_NETWORK,
+                bridge=bridge_name,
+                state=libnncp.BridgeMappings.State.PRESENT.value,
+            )
+        ]),
+    )
+    with libnncp.NodeNetworkConfigurationPolicy(
+        name=bridge_name,
+        desired_state=desired_state,
+        node_selector={WORKER_NODE_LABEL_KEY: ""},
+    ) as nncp:
+        nncp.wait_for_status_success()
+        yield nncp
+
+
+@pytest.fixture(scope="module")
+def nad_localnet_ovs_bridge(
+    namespace_localnet_1: Namespace,
+    vlan_id: int,
+) -> Generator[netattachdef.NetworkAttachmentDefinition]:
+    with localnet_nad(
+        namespace=namespace_localnet_1.name,
+        name=LOCALNET_OVS_BRIDGE_NETWORK,
+        vlan_id=vlan_id,
+        network_name=LOCALNET_OVS_BRIDGE_NETWORK,
+    ) as nad:
+        yield nad
+
+
+@pytest.fixture(scope="module")
+def vm_ovs_bridge_localnet_1(
+    ipv4_localnet_address_pool: Generator[str],
+    nad_localnet_ovs_bridge: netattachdef.NetworkAttachmentDefinition,
+) -> Generator[BaseVirtualMachine]:
+    with localnet_vm(
+        namespace=nad_localnet_ovs_bridge.namespace,
+        name="localnet-ovs-vm1",
+        nad_name=nad_localnet_ovs_bridge.name,
+        cidr=next(ipv4_localnet_address_pool),
+        spec_logical_network=LOCALNET_OVS_BRIDGE_NETWORK,
+    ) as vm:
+        yield vm
+
+
+@pytest.fixture(scope="module")
+def vm_ovs_bridge_localnet_2(
+    ipv4_localnet_address_pool: Generator[str],
+    nad_localnet_ovs_bridge: netattachdef.NetworkAttachmentDefinition,
+) -> Generator[BaseVirtualMachine]:
+    with localnet_vm(
+        namespace=nad_localnet_ovs_bridge.namespace,
+        name="localnet-ovs-vm2",
+        nad_name=nad_localnet_ovs_bridge.name,
+        cidr=next(ipv4_localnet_address_pool),
+        spec_logical_network=LOCALNET_OVS_BRIDGE_NETWORK,
+    ) as vm:
+        yield vm
+
+
+@pytest.fixture(scope="module")
+def ovs_bridge_server_client_localnet_vms(
+    vm_ovs_bridge_localnet_1: BaseVirtualMachine, vm_ovs_bridge_localnet_2: BaseVirtualMachine
+) -> Generator[tuple[BaseVirtualMachine, BaseVirtualMachine]]:
+    server_vm, client_vm = run_vms(vms=(vm_ovs_bridge_localnet_1, vm_ovs_bridge_localnet_2))
+    yield (server_vm, client_vm)
+
+
+@pytest.fixture()
+def localnet_ovs_bridge_server(
+    ovs_bridge_server_client_localnet_vms: tuple[BaseVirtualMachine, BaseVirtualMachine],
+) -> Generator[Server]:
+    with create_traffic_server(vm=ovs_bridge_server_client_localnet_vms[0]) as server:
+        assert server.is_running()
+        yield server
+
+
+@pytest.fixture()
+def localnet_ovs_bridge_client(
+    ovs_bridge_server_client_localnet_vms: tuple[BaseVirtualMachine, BaseVirtualMachine],
+) -> Generator[Client]:
+    with create_traffic_client(
+        server_vm=ovs_bridge_server_client_localnet_vms[0],
+        client_vm=ovs_bridge_server_client_localnet_vms[1],
+        network_name=LOCALNET_OVS_BRIDGE_NETWORK,
     ) as client:
         assert client.is_running()
         yield client
--- tests/network/localnet/liblocalnet.py
+++ tests/network/localnet/liblocalnet.py
@@ -1,15 +1,37 @@
 from libs.net import netattachdef
-from libs.net.vmspec import add_network_interface, add_volume_disk
+from libs.net.traffic_generator import Client, Server
+from libs.net.vmspec import IP_ADDRESS, add_network_interface, add_volume_disk, lookup_iface_status
 from libs.vm.affinity import new_pod_anti_affinity
 from libs.vm.factory import base_vmspec, fedora_vm
 from libs.vm.spec import CloudInitNoCloud, Interface, Metadata, Multus, Network
 from libs.vm.vm import BaseVirtualMachine, cloudinitdisk_storage
 from tests.network.libs import cloudinit
 
-NETWORK_NAME = "localnet-network"
+_IPERF_SERVER_PORT = 5201
 
 
-def localnet_vm(namespace: str, name: str, network: str, cidr: str) -> BaseVirtualMachine:
+def run_vms(vms: tuple[BaseVirtualMachine, ...]) -> tuple[BaseVirtualMachine, ...]:
+    for vm in vms:
+        vm.start()
+    for vm in vms:
+        vm.wait_for_ready_status(status=True)
+        vm.vmi.wait_for_condition(condition=vm.Condition.Type.AGENT_CONNECTED, status=vm.Condition.Status.TRUE)
+    return vms
+
+
+def create_traffic_server(vm: BaseVirtualMachine) -> Server:
+    return Server(vm=vm, port=_IPERF_SERVER_PORT)
+
+
+def create_traffic_client(server_vm: BaseVirtualMachine, client_vm: BaseVirtualMachine, network_name: str) -> Client:
+    return Client(
+        vm=client_vm,
+        server_ip=lookup_iface_status(vm=server_vm, iface_name=network_name)[IP_ADDRESS],
+        server_port=_IPERF_SERVER_PORT,
+    )
+
+
+def localnet_vm(namespace: str, name: str, nad_name: str, cidr: str, spec_logical_network: str) -> BaseVirtualMachine:
     """
     Create a Fedora-based Virtual Machine connected to a given localnet network with a static IP configuration.
 
@@ -22,8 +44,9 @@ def localnet_vm(namespace: str, name: str, network: str, cidr: str) -> BaseVirtu
     Args:
         namespace (str): The namespace where the VM should be created.
         name (str): The name of the VM.
-        network (str): The name of the Multus network to attach.
+        nad_name (str): The name of the Multus network to attach.
         cidr (str): The CIDR address to assign to the VM's interface.
+        spec_logical_network (str): The name of the localnet network to attach.
 
     Returns:
         BaseVirtualMachine: The configured VM object ready for creation.
@@ -37,8 +60,8 @@ def localnet_vm(namespace: str, name: str, network: str, cidr: str) -> BaseVirtu
 
     vmi_spec = add_network_interface(
         vmi_spec=vmi_spec,
-        network=Network(name=NETWORK_NAME, multus=Multus(networkName=network)),
-        interface=Interface(name=NETWORK_NAME, bridge={}),
+        network=Network(name=spec_logical_network, multus=Multus(networkName=nad_name)),
+        interface=Interface(name=spec_logical_network, bridge={}),
     )
 
     netdata = cloudinit.NetworkData(ethernets={"eth0": cloudinit.EthernetDevice(addresses=[cidr])})
@@ -57,12 +80,26 @@ def localnet_vm(namespace: str, name: str, network: str, cidr: str) -> BaseVirtu
     return fedora_vm(namespace=namespace, name=name, spec=spec)
 
 
-def localnet_nad(namespace: str, name: str, vlan_id: int) -> netattachdef.NetworkAttachmentDefinition:
+def localnet_nad(
+    namespace: str, name: str, vlan_id: int, network_name: str
+) -> netattachdef.NetworkAttachmentDefinition:
+    """
+    Create a Network Attachment Definition for a localnet network.
+
+    Args:
+        namespace (str): The namespace for the NetworkAttachmentDefinition.
+        name (str): The name of the NetworkAttachmentDefinition.
+        vlan_id (int): The VLAN ID for the network.
+        network_name (str): The name to use for the network.
+
+    Returns:
+        NetworkAttachmentDefinition: The configured NetworkAttachmentDefinition object.
+    """
     return netattachdef.NetworkAttachmentDefinition(
         namespace=namespace,
         name=name,
         config=netattachdef.NetConfig(
-            NETWORK_NAME,
+            network_name,
             [
                 netattachdef.CNIPluginOvnK8sConfig(
                     topology=netattachdef.CNIPluginOvnK8sConfig.Topology.LOCALNET.value,
