bc9412e836d9858b69d68fef8460e04f9f8484d4
Qixuan Wang <qwang@redhat.com>
2025-06-19 14:53:47 +0800

    [Chaos][GPFS] Add GPFS mark for skipping ODF cases in GPFS clusters (#1228)
    So all cases suitable for running with GPFS can be selected by `-m gpfs`
    
    Signed-off-by: Qixuan Wang <qwang@redhat.com>

--- tests/chaos/standard/test_standard.py
+++ tests/chaos/standard/test_standard.py
@@ -15,11 +15,11 @@ from utilities.virt import VirtualMachineForTests, running_vm
 
 pytestmark = [
     pytest.mark.chaos,
-    pytest.mark.gpfs,
     pytest.mark.usefixtures("chaos_namespace", "cluster_monitoring_process", "skip_on_aws_cluster"),
 ]
 
 
+@pytest.mark.gpfs
 @pytest.mark.parametrize(
     "chaos_vms_list_rhel9, pod_deleting_process",
     [
@@ -53,6 +53,7 @@ def test_pod_delete_openshift_apiserver(
         running_vm(vm=vm, wait_for_interfaces=False, check_ssh_connectivity=False)
 
 
+@pytest.mark.gpfs
 @pytest.mark.parametrize(
     "rebooted_control_plane_node",
     [
@@ -115,6 +116,7 @@ def test_odf_storage_outage(
     chaos_vm_rhel9_with_dv.wait_for_specific_status(status=VirtualMachine.Status.RUNNING, timeout=TIMEOUT_2MIN)
 
 
+@pytest.mark.gpfs
 @pytest.mark.parametrize(
     "chaos_worker_background_process",
     [
@@ -150,6 +152,7 @@ def test_host_io_stress(
     assert chaos_worker_background_process.exitcode == 0, "Background process execution failed"
 
 
+@pytest.mark.gpfs
 @pytest.mark.usefixtures("deleted_pod_by_name_prefix")
 @pytest.mark.parametrize("chaos_vms_instancetype_list", [pytest.param({"number_of_vms": 3})], indirect=True)
 class TestVMInstanceTypeOperationsPodDelete:
