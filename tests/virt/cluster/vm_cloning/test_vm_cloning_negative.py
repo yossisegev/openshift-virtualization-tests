e76713164bdf51c02c01ebedbb4c67a6aa6688da
vsibirsk <57763370+vsibirsk@users.noreply.github.com>
2025-06-26 20:35:59 +0300

    [virt] update vm cloning tests (#1304)
    since cloning from snapshot won't be fixed in 4.19
    split test to check 2 different flows for cloning
    from vm and vm snapshot

--- tests/virt/cluster/vm_cloning/test_vm_cloning_negative.py
+++ tests/virt/cluster/vm_cloning/test_vm_cloning_negative.py
@@ -1,6 +1,7 @@
 import logging
 
 import pytest
+from kubernetes.client import ApiException
 from ocp_resources.virtual_machine_clone import VirtualMachineClone
 
 from tests.virt.cluster.vm_cloning.utils import wait_cloning_job_source_not_exist_reason
@@ -16,21 +17,9 @@ LOGGER = logging.getLogger(__name__)
             marks=pytest.mark.polarion("CNV-10302"),
             id="VirtualMachine_as_source",
         ),
-        pytest.param(
-            {
-                "source_name": "non-existing-vm-snapshot",
-                "source_kind": "VirtualMachineSnapshot",
-            },
-            marks=[
-                pytest.mark.polarion("CNV-10303"),
-                # TODO: this won't be backported to 4.19; need to remove test after 4.19 branch created
-                pytest.mark.jira("CNV-42213", run=False),
-            ],
-            id="VirtualMachineSnapshot_as_source",
-        ),
     ],
 )
-def test_cloning_job_if_source_not_exist_negative(namespace, cloning_job_bad_params):
+def test_cloning_job_if_source_vm_not_exist_negative(namespace, cloning_job_bad_params):
     with VirtualMachineClone(
         name="clone-job-negative-test",
         namespace=namespace.name,
@@ -38,3 +27,30 @@ def test_cloning_job_if_source_not_exist_negative(namespace, cloning_job_bad_par
         source_kind=cloning_job_bad_params["source_kind"],
     ) as vmc:
         wait_cloning_job_source_not_exist_reason(vmc=vmc)
+
+
+@pytest.mark.parametrize(
+    "cloning_job_bad_params",
+    [
+        pytest.param(
+            {
+                "source_name": "non-existing-vm-snapshot",
+                "source_kind": "VirtualMachineSnapshot",
+            },
+            marks=pytest.mark.polarion("CNV-10303"),
+            id="VirtualMachineSnapshot_as_source",
+        ),
+    ],
+)
+def test_cloning_job_if_source_vm_snapshot_not_exist_negative(namespace, cloning_job_bad_params):
+    with pytest.raises(
+        ApiException,
+        match=rf".* {cloning_job_bad_params['source_kind']} {cloning_job_bad_params['source_name']} does not exist .*",
+    ):
+        with VirtualMachineClone(
+            name="clone-job-negative-test",
+            namespace=namespace.name,
+            source_name=cloning_job_bad_params["source_name"],
+            source_kind=cloning_job_bad_params["source_kind"],
+        ):
+            pytest.fail("Cloning job created with non-existing source")
