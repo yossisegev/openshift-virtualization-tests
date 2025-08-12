cfe10103e674b72bc450c45d69f876ec688156a4
Jenia Peimer <86722603+jpeimer@users.noreply.github.com>
2025-08-12 15:33:19 +0300

    Storage: remove duplicate public registry test (#1750)

--- tests/storage/cdi_import/test_import_registry.py
+++ tests/storage/cdi_import/test_import_registry.py
@@ -77,12 +77,7 @@ def test_public_registry_multiple_data_volume(
         pytest.param(
             "import-public-registry-no-content-type-dv",
             None,
-            marks=(pytest.mark.polarion("CNV-2195")),
-        ),
-        pytest.param(
-            "import-public-registry-empty-content-type-dv",
-            "",
-            marks=(pytest.mark.polarion("CNV-2197"), pytest.mark.smoke()),
+            marks=(pytest.mark.polarion("CNV-2195"), pytest.mark.smoke()),
         ),
         pytest.param(
             "import-public-registry-quay-dv",
@@ -92,7 +87,6 @@ def test_public_registry_multiple_data_volume(
     ],
     ids=[
         "import-public-registry-no-content-type-dv",
-        "import-public-registry-empty-content-type-dv",
         "import-public-registry-quay-dv",
     ],
 )
@@ -117,7 +111,6 @@ def test_public_registry_data_volume(
             vm_name="fedora-vm-from-dv",
             os_flavor=OS_FLAVOR_FEDORA,
             memory_guest=Images.Fedora.DEFAULT_MEMORY_SIZE,
-            wait_for_cloud_init=True,
         ) as vm_dv:
             check_disk_count_in_vm(vm=vm_dv)
 
@@ -140,7 +133,6 @@ def test_public_registry_data_volume_low_capacity(namespace, storage_class_name_
         dv_name=dv_param["dv_name"],
         namespace=namespace.name,
         url=dv_param["url"],
-        content_type="",
         size="16Mi",
         storage_class=dv_param["storage_class"],
     ) as dv:
@@ -164,7 +156,6 @@ def test_public_registry_data_volume_low_capacity(namespace, storage_class_name_
             vm_name="fedora-vm-from-dv",
             os_flavor=OS_FLAVOR_FEDORA,
             memory_guest=Images.Fedora.DEFAULT_MEMORY_SIZE,
-            wait_for_cloud_init=True,
         ) as vm_dv:
             check_disk_count_in_vm(vm=vm_dv)
 
