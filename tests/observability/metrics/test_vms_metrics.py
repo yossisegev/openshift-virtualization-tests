8cf21588d0ed6fb1901dd22b58cf068ba30d22e0
Ohad Revah <orevah@redhat.com>
2025-08-26 20:19:45 +0300

    Change implementation of TestVMICountMetric (#1852)
    TestVMICountMetric used old function and it was
    duplicate of the function that validate metric value.
    Also it waited too short for the metric to update which led
    to failure of the test in the last run.

--- tests/observability/metrics/test_vms_metrics.py
+++ tests/observability/metrics/test_vms_metrics.py
@@ -6,7 +6,6 @@ import pytest
 from ocp_resources.datavolume import DataVolume
 from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
 from ocp_resources.virtual_machine import VirtualMachine
-from ocp_resources.virtual_machine_instance import VirtualMachineInstance
 from ocp_resources.virtual_machine_instance_migration import (
     VirtualMachineInstanceMigration,
 )
@@ -148,66 +147,6 @@ def vm_metric_1_vmim(vm_metric_1):
     ) as vmim:
         vmim.wait_for_status(status=vmim.Status.RUNNING, timeout=TIMEOUT_3MIN)
         yield
-
-
-@pytest.fixture(scope="class")
-def vm_metric_2(namespace, unprivileged_client):
-    vm_name = "vm-metrics-2"
-    with VirtualMachineForTests(
-        name=vm_name,
-        namespace=namespace.name,
-        body=fedora_vm_body(name=vm_name),
-        client=unprivileged_client,
-    ) as vm:
-        running_vm(vm=vm, wait_for_interfaces=False, check_ssh_connectivity=False)
-        yield vm
-
-
-@pytest.fixture(scope="class")
-def number_of_running_vmis(admin_client):
-    return len(list(VirtualMachineInstance.get(dyn_client=admin_client)))
-
-
-def check_vmi_metric(prometheus):
-    response = prometheus.query(query="cnv:vmi_status_running:count")
-    assert response["status"] == "success"
-    return sum(int(node["value"][1]) for node in response["data"]["result"])
-
-
-def check_vmi_count_metric(expected_vmi_count, prometheus):
-    LOGGER.info(f"Check VMI metric expected: {expected_vmi_count}")
-    samples = TimeoutSampler(
-        wait_timeout=100,
-        sleep=5,
-        func=check_vmi_metric,
-        prometheus=prometheus,
-    )
-    for sample in samples:
-        if sample == expected_vmi_count:
-            return True
-
-
-class TestVMICountMetric:
-    @pytest.mark.polarion("CNV-3048")
-    def test_vmi_count_metric_increase(
-        self,
-        prometheus,
-        number_of_running_vmis,
-        vm_metric_1,
-        vm_metric_2,
-    ):
-        assert check_vmi_count_metric(number_of_running_vmis + 2, prometheus)
-
-    @pytest.mark.polarion("CNV-3589")
-    def test_vmi_count_metric_decrease(
-        self,
-        prometheus,
-        number_of_running_vmis,
-        vm_metric_1,
-        vm_metric_2,
-    ):
-        vm_metric_2.stop(wait=True)
-        assert check_vmi_count_metric(number_of_running_vmis + 1, prometheus)
 
 
 class TestVMStatusLastTransitionMetrics:
