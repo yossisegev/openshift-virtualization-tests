4523b098c82c02a2c941931157b1ef2948fb399b
Harel Meir <hmeir@redhat.com>
2025-09-07 18:54:45 +0300

    [IUO] Remove jira CNV-64424 (#1930)
    * [IUO] Remove jira CNV-64424
    
    The bug was fixed at 4.20, should align the automation
    
    Signed-off-by: Harel Meir <hmeir@redhat.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Harel Meir <hmeir@redhat.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/install_upgrade_operators/crds_cluster_readers_role/test_crds_cluster_readers_role.py
+++ tests/install_upgrade_operators/crds_cluster_readers_role/test_crds_cluster_readers_role.py
@@ -6,48 +6,24 @@ import pytest
 from ocp_resources.custom_resource_definition import CustomResourceDefinition
 from ocp_resources.resource import Resource
 
-from utilities.infra import is_jira_open
-
 LOGGER = logging.getLogger(__name__)
-MTV_VOLUME_POPULATOR_CRDS = [
-    f"openstackvolumepopulators.forklift.cdi.{Resource.ApiGroup.KUBEVIRT_IO}",
-    f"ovirtvolumepopulators.forklift.cdi.{Resource.ApiGroup.KUBEVIRT_IO}",
-]
 
-
-# Tests not marked as `conformance` because they rely on access to Jira
-pytestmark = [pytest.mark.sno, pytest.mark.gating, pytest.mark.arm64, pytest.mark.s390x]
+pytestmark = [pytest.mark.sno, pytest.mark.gating, pytest.mark.arm64, pytest.mark.s390x, pytest.mark.conformance]
 
 
 @pytest.fixture()
 def crds(admin_client):
-    crds_to_check = []
-    # TODO: once bug is removed, test should be marked as `conformance`
-    bug_status = is_jira_open(jira_id="CNV-64424")
-    for crd in CustomResourceDefinition.get(dyn_client=admin_client):
-        if bug_status and crd.name in MTV_VOLUME_POPULATOR_CRDS:
-            continue
-        if any([
-            crd.name.endswith(suffix)
-            for suffix in [
-                Resource.ApiGroup.KUBEVIRT_IO,
-                Resource.ApiGroup.NMSTATE_IO,
-            ]
-        ]):
-            crds_to_check.append(crd)
-    return crds_to_check
+    target_suffixes = (Resource.ApiGroup.KUBEVIRT_IO, Resource.ApiGroup.NMSTATE_IO)
+    return [crd for crd in CustomResourceDefinition.get(dyn_client=admin_client) if crd.name.endswith(target_suffixes)]
 
 
 @pytest.mark.polarion("CNV-8263")
 def test_crds_cluster_readers_role(crds):
-    LOGGER.info(f"CRds: {crds}")
     cluster_readers = "system:cluster-readers"
-    cannot_read = []
+    unreadable_crds = []
     for crd in crds:
         can_read = check_output(shlex.split(f"oc adm policy who-can get {crd.name}"))
         if cluster_readers not in str(can_read):
-            cannot_read.append(crd.name)
+            unreadable_crds.append(crd.name)
 
-    if cannot_read:
-        cannot_read_str = "\n".join(cannot_read)
-        pytest.fail(reason=f"The following crds are missing {cluster_readers} role:\n{cannot_read_str}")
+    assert not unreadable_crds, f"The following crds are missing {cluster_readers} role: {unreadable_crds}"
