eca39e9c3d5d39dd747146936a5e803030851bcc
Jose Manuel Castano <73908807+josemacassan@users.noreply.github.com>
2025-10-28 12:58:31 +0100

    [4.19] Storage gating: test_vmexport_snapshot_manifests change pod execution logic (#2407)
    * Storage gating: test_vmexport_snapshot_manifests change pod execution logic (#2247)
    
    * Change pvc check logic to match end-user workflow
    
    * Refactor test_vm_export_snapshot_manifest.
    
    * Change rhel for cirros into vmexport test and check persistent data.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix codeRabbit suggestion.
    
    * Chage PR reviews suggestions.
    
    * Fix pre-commit errors.
    
    * Remove reduntant variables in test test_vmexport_snapshot_manifest
    
    * Fix redundant variables after PR review
    
    * Fix some comments suggestions after PR review.
    
    * Remove comment.
    
    * Change form of running ssh commands into test test_vmexport_snapshot_manifest.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * Remove pytest mark s390x
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/storage/vm_export/utils.py
+++ tests/storage/vm_export/utils.py
@@ -7,37 +7,10 @@ import logging
 import shlex
 
 import yaml
-from ocp_resources.datavolume import DataVolume
-from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
-from ocp_resources.storage_profile import StorageProfile
 from ocp_resources.virtual_machine import VirtualMachine
 from pyhelper_utils.shell import run_command
 
-from utilities.storage import PodWithPVC, get_containers_for_pods_with_pvc
-
 LOGGER = logging.getLogger(__name__)
-
-
-def get_pvc_sha256sum(pvc_name, pvc_namespace):
-    pvc = PersistentVolumeClaim(namespace=pvc_namespace, name=pvc_name)
-    volume_mode = StorageProfile(name=pvc.instance.spec.storageClassName).instance.status["claimPropertySets"][0][
-        "volumeMode"
-    ]
-    with PodWithPVC(
-        namespace=pvc_namespace,
-        name=f"{pvc_name}-pod",
-        pvc_name=pvc_name,
-        containers=get_containers_for_pods_with_pvc(volume_mode=volume_mode, pvc_name=pvc_name),
-    ) as pod:
-        pod.wait_for_status(status=pod.Status.RUNNING)
-        pvc_disk_img = "/pvc/disk.img"
-        checksum = "sha256sum"
-        command = (
-            f"bash -c 'head -c 1000000 {pvc_disk_img} | {checksum}'"
-            if volume_mode == DataVolume.VolumeMode.BLOCK
-            else f"{checksum} {pvc_disk_img}"
-        )
-        return pod.execute(command=shlex.split(command))
 
 
 def get_manifest_from_vmexport(vmexport_cert_file, url, token, kind, namespace_vmexport_target=None):
