17684f11269151ffb9dfb466a3f60bcb47e03260
Sergei Volkov <153028767+servolkov@users.noreply.github.com>
2025-11-03 13:01:50 +0100

    net: fix network policy tests after wrapper update (#2446)
    NetworkPolicy class has been updated and `pod_selector` becomes
    mandatory even if it's empty. The current fix sets `pod_selector`
    in the child `ApplyNetworkPolicy` class and thus mends
    the network policy tests.
    
    Signed-off-by: Sergei Volkov <sevolkov@redhat.com>

--- tests/network/network_policy/test_network_policy.py
+++ tests/network/network_policy/test_network_policy.py
@@ -21,7 +21,7 @@ pytestmark = pytest.mark.sno
 
 class ApplyNetworkPolicy(NetworkPolicy):
     def __init__(self, name, namespace, ports=None, teardown=True):
-        super().__init__(name=name, namespace=namespace, teardown=teardown)
+        super().__init__(name=name, namespace=namespace, teardown=teardown, pod_selector={})
         self.ports = ports
 
     def to_dict(self):
@@ -31,7 +31,6 @@ class ApplyNetworkPolicy(NetworkPolicy):
             for port in self.ports:
                 _ports.append({"protocol": "TCP", "port": port})
 
-        self.res["spec"] = {"podSelector": {}}
         if _ports:
             self.res["spec"]["ingress"] = [{"ports": _ports}]
 
