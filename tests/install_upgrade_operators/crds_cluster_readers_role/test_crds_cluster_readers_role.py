d5edae53afb7ca7bf168cbafea476f7539a5614a
Harel Meir <hmeir@redhat.com>
2025-11-12 17:28:18 +0200

    [4.19][IUO] add retry to crd.get (#2456)
    ##### Short description:
    Cherry-pick of
    https://github.com/RedHatQE/openshift-virtualization-tests/pull/2394
    ##### More details:
    
    ##### What this PR does / why we need it:
    
    ##### Which issue(s) this PR fixes:
    
    ##### Special notes for reviewer:
    
    ##### jira-ticket:
    <!-- full-ticket-url needs to be provided. This would add a link to the
    pull request to the jira and close it when the pull request is merged
    If the task is not tracked by a Jira ticket, just write "NONE".
    -->
    
    Signed-off-by: Harel Meir <hmeir@redhat.com>

--- tests/install_upgrade_operators/crds_cluster_readers_role/test_crds_cluster_readers_role.py
+++ tests/install_upgrade_operators/crds_cluster_readers_role/test_crds_cluster_readers_role.py
@@ -3,35 +3,43 @@ import shlex
 from subprocess import check_output
 
 import pytest
+from kubernetes.dynamic import DynamicClient
 from ocp_resources.custom_resource_definition import CustomResourceDefinition
 from ocp_resources.resource import Resource
+from timeout_sampler import retry
+
+from utilities.constants import BASE_EXCEPTIONS_DICT, TIMEOUT_3MIN, TIMEOUT_10SEC
 
 LOGGER = logging.getLogger(__name__)
 
 pytestmark = [pytest.mark.sno, pytest.mark.gating, pytest.mark.arm64, pytest.mark.conformance]
 
 
-@pytest.fixture()
-def crds(admin_client):
-    crds_to_check = []
-    for crd in CustomResourceDefinition.get(dyn_client=admin_client):
-        if any([
-            crd.name.endswith(suffix)
-            for suffix in [
-                Resource.ApiGroup.KUBEVIRT_IO,
-                Resource.ApiGroup.NMSTATE_IO,
-            ]
-        ]):
-            crds_to_check.append(crd)
-    return crds_to_check
+@retry(
+    wait_timeout=TIMEOUT_3MIN,
+    sleep=TIMEOUT_10SEC,
+    exceptions_dict=BASE_EXCEPTIONS_DICT,
+)
+def get_cnv_crds(admin_client: DynamicClient) -> list[CustomResourceDefinition]:
+    """
+    Fetch CNV-related CRDs with retry logic to handle large responses and network instability.
+
+    Large CRD listings (10MB+) can trigger ProtocolError/IncompleteRead under unstable
+    network conditions. Retries mitigate transient failures; repeated failures may indicate
+    cluster or network issues.
+    """
+    return [
+        crd
+        for crd in CustomResourceDefinition.get(dyn_client=admin_client)
+        if crd.name.endswith(Resource.ApiGroup.KUBEVIRT_IO)
+    ]
 
 
 @pytest.mark.polarion("CNV-8263")
-def test_crds_cluster_readers_role(crds):
-    LOGGER.info(f"CRds: {crds}")
+def test_crds_cluster_readers_role(admin_client):
     cluster_readers = "system:cluster-readers"
     cannot_read = []
-    for crd in crds:
+    for crd in get_cnv_crds(admin_client=admin_client):
         can_read = check_output(shlex.split(f"oc adm policy who-can get {crd.name}"))
         if cluster_readers not in str(can_read):
             cannot_read.append(crd.name)
