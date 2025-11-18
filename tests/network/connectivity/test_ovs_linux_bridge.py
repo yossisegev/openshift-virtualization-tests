5bd35767f0a651260406aa75be0bada63047b3dd
openshift-virtualization-qe-bot-3 <rnetser+opendatahub-tests-bot3@redhat.com>
2025-11-18 18:50:53 +0200

    CherryPicked: [cnv-4.20] Remove false single_nic marker from bridge test that requires multi NICs (#2720)
    cherry-pick
    https://github.com/RedHatQE/openshift-virtualization-tests/pull/2691
    into cnv-4.20
    
    requested-by yossisegev
    
    Co-authored-by: Yossi Segev <40713576+yossisegev@users.noreply.github.com>

--- tests/network/connectivity/test_ovs_linux_bridge.py
+++ tests/network/connectivity/test_ovs_linux_bridge.py
@@ -51,7 +51,6 @@ class TestConnectivityLinuxBridge:
     @pytest.mark.post_upgrade
     @pytest.mark.polarion("CNV-11125")
     @pytest.mark.ipv6
-    @pytest.mark.single_nic
     def test_ipv6_linux_bridge(
         self,
         fail_if_not_ipv6_supported_cluster,
