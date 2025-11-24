2ee0a0940d9c23dbbaf5fb789c1590b66fa77b13
Asia Khromov <azhivovk@redhat.com>
2025-11-24 12:35:26 +0200

    net, localnet, jumbo frame: Add jumbo fixtures and new test
    Create dedicated fixtures and add new test:
    
    Verify connectivity using localnet and jumbo frames
    over ovs bridge, without fragmentaion
    This test combines the existing localnet and jumbo frame tests
    
    The test includes 2 steps:
    1) Verify jumbo frames are not fragmented with ping
    2) Verify connectivity using iperf3 client-server with jumbo frame
    
    Signed-off-by: Asia Khromov <azhivovk@redhat.com>

--- tests/network/libs/ip.py
+++ tests/network/libs/ip.py
@@ -4,6 +4,9 @@ from typing import Final
 
 _MAX_NUM_OF_RANDOM_OCTETS_PER_SESSION: Final[int] = 16
 _IPV4_ADDRESS_SUBNET_PREFIX_VMI: Final[str] = "172.16"
+TCP_HEADER_SIZE: Final[int] = 20
+IPV4_HEADER_SIZE: Final[int] = 20
+ICMPV4_HEADER_SIZE: Final[int] = 8
 
 
 def random_ipv4_address(net_seed: int, host_address: int) -> str:
--- tests/network/localnet/conftest.py
+++ tests/network/localnet/conftest.py
@@ -12,8 +12,9 @@ from libs.vm.spec import Interface, Multus, Network
 from libs.vm.vm import BaseVirtualMachine
 from tests.network.libs import cloudinit
 from tests.network.libs import cluster_user_defined_network as libcudn
-from tests.network.libs.ip import random_ipv4_address
+from tests.network.libs.ip import IPV4_HEADER_SIZE, TCP_HEADER_SIZE, random_ipv4_address
 from tests.network.localnet.liblocalnet import (
+    _IPERF_SERVER_PORT,
     LINK_STATE_DOWN,
     LOCALNET_BR_EX_INTERFACE,
     LOCALNET_BR_EX_INTERFACE_NO_VLAN,
@@ -356,3 +357,101 @@ def nncp_localnet_on_secondary_node_nic(
 ) -> Generator[libnncp.NodeNetworkConfigurationPolicy]:
     with create_nncp_localnet_on_secondary_node_nic(node_nic_name=(hosts_common_available_ports[-1])) as nncp:
         yield nncp
+
+
+@pytest.fixture(scope="module")
+def nncp_localnet_on_secondary_node_nic_with_jumbo_frame(
+    hosts_common_available_ports: list[str], cluster_hardware_mtu: int
+) -> Generator[libnncp.NodeNetworkConfigurationPolicy]:
+    with create_nncp_localnet_on_secondary_node_nic(
+        node_nic_name=(hosts_common_available_ports[-1]), mtu=cluster_hardware_mtu
+    ) as nncp:
+        yield nncp
+
+
+@pytest.fixture(scope="module")
+def cudn_localnet_ovs_bridge_jumbo_frame(
+    vlan_id: int,
+    cluster_hardware_mtu: int,
+    namespace_localnet_1: Namespace,
+) -> Generator[libcudn.ClusterUserDefinedNetwork]:
+    with localnet_cudn(
+        name=LOCALNET_OVS_BRIDGE_NETWORK,
+        match_labels=LOCALNET_TEST_LABEL,
+        vlan_id=vlan_id,
+        physical_network_name=LOCALNET_OVS_BRIDGE_NETWORK,
+        mtu=cluster_hardware_mtu,
+    ) as cudn:
+        cudn.wait_for_status_success()
+        yield cudn
+
+
+@pytest.fixture(scope="module")
+def vm1_ovs_bridge_localnet_jumbo_frame(
+    namespace_localnet_1: Namespace,
+    ipv4_localnet_address_pool: Generator[str],
+    cudn_localnet_ovs_bridge_jumbo_frame: libcudn.ClusterUserDefinedNetwork,
+    unprivileged_client: DynamicClient,
+) -> Generator[BaseVirtualMachine]:
+    with localnet_vm(
+        namespace=namespace_localnet_1.name,
+        name="localnet-ovs-vm1-jumbo",
+        client=unprivileged_client,
+        networks=[
+            Network(
+                name=LOCALNET_OVS_BRIDGE_INTERFACE, multus=Multus(networkName=cudn_localnet_ovs_bridge_jumbo_frame.name)
+            )
+        ],
+        interfaces=[Interface(name=LOCALNET_OVS_BRIDGE_INTERFACE, bridge={})],
+        network_data=cloudinit.NetworkData(
+            ethernets={PRIMARY_INTERFACE_NAME: cloudinit.EthernetDevice(addresses=[next(ipv4_localnet_address_pool)])}
+        ),
+    ) as vm:
+        yield vm
+
+
+@pytest.fixture(scope="module")
+def vm2_ovs_bridge_localnet_jumbo_frame(
+    namespace_localnet_1: Namespace,
+    ipv4_localnet_address_pool: Generator[str],
+    cudn_localnet_ovs_bridge_jumbo_frame: libcudn.ClusterUserDefinedNetwork,
+    unprivileged_client: DynamicClient,
+) -> Generator[BaseVirtualMachine]:
+    with localnet_vm(
+        namespace=namespace_localnet_1.name,
+        name="localnet-ovs-vm2-jumbo",
+        client=unprivileged_client,
+        networks=[
+            Network(
+                name=LOCALNET_OVS_BRIDGE_INTERFACE, multus=Multus(networkName=cudn_localnet_ovs_bridge_jumbo_frame.name)
+            )
+        ],
+        interfaces=[Interface(name=LOCALNET_OVS_BRIDGE_INTERFACE, bridge={})],
+        network_data=cloudinit.NetworkData(
+            ethernets={PRIMARY_INTERFACE_NAME: cloudinit.EthernetDevice(addresses=[next(ipv4_localnet_address_pool)])}
+        ),
+    ) as vm:
+        yield vm
+
+
+@pytest.fixture(scope="module")
+def ovs_bridge_localnet_running_jumbo_frame_vms(
+    vm1_ovs_bridge_localnet_jumbo_frame: BaseVirtualMachine, vm2_ovs_bridge_localnet_jumbo_frame: BaseVirtualMachine
+) -> Generator[tuple[BaseVirtualMachine, BaseVirtualMachine]]:
+    vm1, vm2 = run_vms(vms=(vm1_ovs_bridge_localnet_jumbo_frame, vm2_ovs_bridge_localnet_jumbo_frame))
+    yield vm1, vm2
+
+
+@pytest.fixture()
+def localnet_ovs_bridge_jumbo_frame_client_and_server_vms(
+    ovs_bridge_localnet_running_jumbo_frame_vms: tuple[BaseVirtualMachine, BaseVirtualMachine],
+    cluster_hardware_mtu: int,
+) -> Generator[tuple[TcpClient, TcpServer], None, None]:
+    with client_server_active_connection(
+        client_vm=ovs_bridge_localnet_running_jumbo_frame_vms[1],
+        server_vm=ovs_bridge_localnet_running_jumbo_frame_vms[0],
+        spec_logical_network=LOCALNET_OVS_BRIDGE_INTERFACE,
+        port=_IPERF_SERVER_PORT,
+        maximum_segment_size=cluster_hardware_mtu - IPV4_HEADER_SIZE - TCP_HEADER_SIZE,
+    ) as (client, server):
+        yield client, server
