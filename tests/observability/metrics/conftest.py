5e146ff87eb2f022f18255bc5dcfaecbaa53a2d6
openshift-virtualization-qe-bot-3 <rnetser+opendatahub-tests-bot3@redhat.com>
2025-11-24 10:52:45 +0200

    CherryPicked: [cnv-4.20] Remove cpu recording rule (#2749)
    cherry-pick
    https://github.com/RedHatQE/openshift-virtualization-tests/pull/2424
    into cnv-4.20
    
    requested-by OhadRevah
    
    Co-authored-by: Ohad Revah <orevah@redhat.com>

--- tests/observability/metrics/conftest.py
+++ tests/observability/metrics/conftest.py
@@ -8,7 +8,6 @@ from ocp_resources.deployment import Deployment
 from ocp_resources.pod import Pod
 from ocp_resources.resource import ResourceEditor
 from ocp_resources.storage_class import StorageClass
-from ocp_resources.virtual_machine import VirtualMachine
 from ocp_resources.virtual_machine_instance_migration import VirtualMachineInstanceMigration
 from pytest_testconfig import py_config
 from timeout_sampler import TimeoutExpiredError, TimeoutSampler
@@ -22,7 +21,6 @@ from tests.observability.metrics.constants import (
 )
 from tests.observability.metrics.utils import (
     SINGLE_VM,
-    ZERO_CPU_CORES,
     create_windows11_wsl2_vm,
     disk_file_system_info,
     enable_swap_fedora_vm,
@@ -31,7 +29,6 @@ from tests.observability.metrics.utils import (
     get_vmi_guest_os_kernel_release_info_metric_from_vm,
     metric_result_output_dict_by_mountpoint,
     vnic_info_from_vm_or_vmi,
-    wait_for_metric_vmi_request_cpu_cores_output,
 )
 from tests.observability.utils import validate_metrics_value
 from tests.utils import create_vms
@@ -106,44 +103,6 @@ def unique_namespace(admin_client, unprivileged_client):
     """
     namespace_name = unique_name(name="key-metrics")
     yield from create_ns(admin_client=admin_client, unprivileged_client=unprivileged_client, name=namespace_name)
-
-
-@pytest.fixture()
-def stopped_metrics_vm(running_metric_vm):
-    running_metric_vm.stop(wait=True)
-    yield
-
-
-@pytest.fixture()
-def starting_metrics_vm(running_metric_vm):
-    running_metric_vm.start(wait=True)
-    yield
-
-
-@pytest.fixture()
-def paused_metrics_vm(running_metric_vm):
-    running_metric_vm.privileged_vmi.pause(wait=True)
-    yield
-
-
-@pytest.fixture(scope="module")
-def initial_metric_cpu_value_zero(prometheus):
-    wait_for_metric_vmi_request_cpu_cores_output(prometheus=prometheus, expected_cpu=ZERO_CPU_CORES)
-
-
-@pytest.fixture(scope="class")
-def error_state_vm(unique_namespace, unprivileged_client):
-    vm_name = "vm-in-error-state"
-    with VirtualMachineForTests(
-        name=vm_name,
-        namespace=unique_namespace.name,
-        body=fedora_vm_body(name=vm_name),
-        client=unprivileged_client,
-        node_selector=get_node_selector_dict(node_selector="non-existent-node"),
-    ) as vm:
-        vm.start()
-        vm.wait_for_specific_status(status=VirtualMachine.Status.ERROR_UNSCHEDULABLE)
-        yield
 
 
 @pytest.fixture(scope="module")
