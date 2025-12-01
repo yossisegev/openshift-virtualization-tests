5007a1e098d3d9fffeb331656cf7ac5f2bf912ba
Yossi Segev <ysegev@redhat.com>
2025-12-01 15:33:57 +0200

    Mark new jumbo tests module as special_infra
    Jumbo suppurt is considered as special_infra, and its tests are executed on the dedicated full-capabilities lanes.
    Not having the special_infra marker on these tests leads to attempting to execute these tests on clusters that
    don't support jumbo, and fail on sanity.

--- tests/network/localnet/test_jumbo_frames.py
+++ tests/network/localnet/test_jumbo_frames.py
@@ -7,6 +7,12 @@ from utilities.network import get_vmi_ip_v4_by_name
 from utilities.virt import vm_console_run_commands
 
 
+pytestmark = [
+    pytest.mark.special_infra,
+    pytest.mark.jumbo_frame,
+]
+
+
 @pytest.mark.polarion("CNV-12349")
 @pytest.mark.jumbo_frame
 @pytest.mark.usefixtures("nncp_localnet_on_secondary_node_nic_with_jumbo_frame")
